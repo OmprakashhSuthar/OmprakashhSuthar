d13b037363bee628d6fa6734783315fac8566f1d
Omprakash Suthar <132443285+OmprakashhSuthar@users.noreply.github.com>
2023-05-12 18:48:28 +0530

    practiceD8

--- js_jquery/script.js
+++ js_jquery/script.js
@@ -22,13 +22,19 @@ $(document).ready(function () {
     //     // $(this).after('x')
     //     $(this).append('<span class="glyphicon glyphicon-ok"></span>')
     // })
+    //Adding list value dynamically using input box 
     // $('button').click(function () {
     //     console.log('clicked')
     //     let a = $('#input').val();
     //     console.log(a)
     //     $('ol').append('<li>' + a + '</li>')
     //     $('#input').val('')
+
     // })
+    // $('ol').on('click','li',function () {
+    //     $(this).css('background-color','green')
+    // })
+
     // $('li').on('mouseclick',function(){
     //     // console.log('ompraksh')
     //     $(this).prepend('om')
@@ -41,12 +47,40 @@ $(document).ready(function () {
     //     let value = $('li').text();
     //     $(this).text(value);
     // })
-    $('.p1').mouseover(function(e){
-        $('.p1').text('mouseover')
-    }).mouseout(function(e){
-        console.log(e)
-        let val = $('.p1').text();
-        console.log(val)
-        $('.p1').text(val);
+    // $('.p1').mouseover(function(e){
+    //     $('.p1').text('mouseover')
+    // }).mouseout(function(e){
+    //     console.log(e)
+    //     let val = $('.p1').text();
+    //     console.log(val)
+    //     $('.p1').text(val);
+    // })
+
+    // $('ol li').click(function(){
+    //     $(this).children().css('color','red')
+    // });
+    // $('ol li').children().last().css('color','')
+    // $('.container').children().last().css('color', 'red')
+
+
+    // $('p').click(function () {
+    //     $(this).removeClass('p1');
+    // })
+    //Adding and removing class in DOM
+    // $('p').click(function(){
+    //     if($(this).children().hasClass('p1')){
+    //         $(this).removeClass('p1')
+    //         $(this).hide('p1')
+    //         $(this).css('padding','10px')
+    //     }else{
+    //         $(this).addClass('p');
+    //         $(this).css('padding','10px')
+
+    //     }
+    // })
+    //Hide and show function in jquery
+    $('p').click(function () {
+        $(this).toggle(2000)
     })
 })
+    
